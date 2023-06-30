0a7da0c86c0f0541369ae61b64102b53757b570e
radRoy <daniel.walther@uzh.ch>
2023-06-30 11:52:12 +0200

    generic commit message, Fri 11:52:12 30.06.2023.

--- createDirs.sh
+++ createDirs.sh
@@ -1,9 +1,18 @@
-i=0
+i = 0
+dir = $(date '+chpt-%y%m%d-')
+dirNum = dir + $i
+echo dirNum
 
 while [ $i -le 2 ]
 do
-
-  if [ dir-$i exists not ]; then
+  # if [ directory <argument> exists]
+  if [ -d "$1" ]; then
+    ((i++)); continue
+  else
+    
+  fi
+  
+if [ dir-$i exists not ]; then
     ((i++))
     continue
 
---
+++ stringConcatenation.sh
@@ -0,0 +1,5 @@
+i=0
+echo $i
+
+dir=date
+echo $dir
