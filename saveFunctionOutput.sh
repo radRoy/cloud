c55c3cad5f3bbd9cd9c5724f320fadf9233f63e0
radRoy <daniel.walther@uzh.ch>
2023-09-07 16:09:15 +0200

    Update saveFunctionOutput.sh

--- saveFunctionOutput.sh
+++ saveFunctionOutput.sh
@@ -2,7 +2,7 @@
 # tests assignment of function output to a variable inside a bash script
 
 # importing functions from this file
-source ../returnChptDirs.sh
+source ./returnChptDirs.sh
 
 # assigning output value of this function 'checkpoint' to a variable 'checkdir'
 checkdir=$(checkpoint)
