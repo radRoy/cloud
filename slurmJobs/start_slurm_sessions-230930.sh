5d88a9cc0e09c4c48953dd7380c5736aeb81793a
radRoy <daniel.walther@uzh.ch>
2023-09-30 16:22:31 +0200

    Update start_slurm_sessions-230930.sh

--- slurmJobs/start_slurm_sessions-230930.sh
+++ slurmJobs/start_slurm_sessions-230930.sh
@@ -9,15 +9,15 @@ source ./getNextSession.sh
 # defining the number of sessions to be run (and prepared for) today (230930)
 n=$((18))
 
-# getting today's date
-date=$(date '+%y%m%d')
+# # getting today's date
+# date=$(date '+%y%m%d')
 
 # iteratively starting/submitting all prepared slurm jobs
 while ((i < n)); do
     
-    session="${date}-$i"
+    session=$(nextSession)
     echo "i: $i, session: ${session}"
-    echo " bash slurmJobs/start_slurm_session.sh $session"
+    echo " bash ./slurmJobs/start_slurm_session.sh $session"
 
     ((i++))
 done
