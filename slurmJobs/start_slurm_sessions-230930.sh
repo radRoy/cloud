480874b89a887461271770083d233e6c76de5f18
radRoy <daniel.walther@uzh.ch>
2023-09-30 16:09:31 +0200

    Update start_slurm_sessions-230930.sh

--- slurmJobs/start_slurm_sessions-230930.sh
+++ slurmJobs/start_slurm_sessions-230930.sh
@@ -13,7 +13,7 @@ n=18
 date=$(date '+%y%m%d')
 
 # iteratively starting/submitting all prepared slurm jobs
-while [ $i -lt $n ]; do
+while (( $i < $n )); do
     
     session="${date}-${i}"
     echo "i: ${i}, session: ${session}"
