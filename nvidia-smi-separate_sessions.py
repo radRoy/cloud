5a272ed5da81190f61d782f8ce28655739bb6881
radRoy <daniel.walther@uzh.ch>
2023-10-04 12:17:08 +0200

    refine nvidia-smi calc script

--- nvidia-smi-separate_sessions.py
+++ nvidia-smi-separate_sessions.py
@@ -13,125 +13,191 @@ function: extract the max used VRAM for different runs from one collective nvidi
 """
 
 
-""" OPEN FILE """
+import sys
+from typing import List
 
 
-print("")  # add a newline at start of script output
-with open(file="C:/Users/Dancer/Documents/cluster/data/outputs/chpt-230901-0/nvidia-smi.log", mode="r") as file:
+def process_nvidia_smi_line(line: str):
+    """
+    Extracts gpu name, used memory, free memory, and calculates memory capacity from a line of nvidia-smi.log output text.
 
-    # read the text into a list so it can be read multiple times
-    readlines = file.readlines()
-    lines = []
-    for line in readlines:
-        lines.append(line)
+    Args:
+        line: str: A line from file.readlines() from a cloud/outputs/.../nvidia-smi.log file, e.g.: 'NVIDIA A100-SXM4-80GB, 0 MiB, 81052 MiB'.
 
-    # explore the text file
-    n_lines = len(lines)
-    for line in lines[0:2]:
-        print(line, end="")
-    print("")
-    print(lines[2].split(" "))
-    # gpu = line.split(" ")[0] + " " + line.split(" ")[1].strip("-")
-    # used = int(line.split(" ")[2])
-    # free = int(line.split(" ")[4])
-    gpu = line.split(" ")[0] + " " + line.split(" ")[1].strip(",")
-    used = int(line.split(" ")[2])
-    free = int(line.split(" ")[4])
-    print(type(gpu), gpu.strip(","))
-    print(type(used), used)
-    print(type(free), free)
-    print("")
+    Returns: A tuple containing the given line's str variables for (gpu name, used memory, free memory, memory capacity).
 
-    # put gpu name, used memory, and free memory in a pandas data frame // or in any or list format, doesn't matter
-    columns = ("gpu", "memory_used_MiB", "memory_free_MiB")
-    gpus = []
-    mem_used_MiB = []
-    mem_free_MiB = []
-    mem_cap_MiB = []
-    mem_cap_unique = []
+    """
 
-    # first iteration (first line)
-    # extract character values to variables
-    gpu = lines[1].split(" ")[0] + " " + lines[1].split(" ")[1].strip(",")
-    used = int(lines[1].split(" ")[2])
-    free = int(lines[1].split(" ")[4])
+    """ TESTING
+    print(f"process_nvidia_smi_line(line)'s line: '{line}'.")
+    """
+
+    split_line: list[str] = line.split(" ")
+    gpu = split_line[0] + " " + split_line[1].strip(",")
+    used = int(split_line[2])
+    free = int(split_line[4])
     cap = used + free
-    # append variables to respective lists
-    gpus.append(gpu)
-    mem_used_MiB.append(used)
-    mem_free_MiB.append(free)
-    mem_cap_MiB.append(cap)
-    # append unique memory capacity values to its respective list
-    if cap not in mem_cap_unique:
-        mem_cap_unique.append(cap)
 
-    # print(f"last line:\n {lines[-1]}\n")
-        # 'Tesla V100-SXM2'
-    lines.pop(-1)
-    # print(f"last line:\n {lines[-1]}\n")
-        # 'Tesla V100-SXM2-32GB, 0 MiB, 32500 MiB'
+    return gpu, used, free, cap
 
-    # all subsequent iterations (lines)
-    for i, line in enumerate(lines[2:]):
-        # extract character values to variables
-        #split_line = line.split(" ")
+
+def main(arg="C:/Users/Dancer/Documents/cluster/data/outputs/chpt-230901-0/nvidia-smi.log"):
+    """ OPEN FILE """
+
+    with open(file=arg, mode="r") as file:
+
+        # read the text into a list so it can be read multiple times
+        readlines = file.readlines()
+        lines = []
+        for line in readlines:
+            lines.append(line)
+
+        """ TESTING
+        # explore the text file
+        n_lines = len(lines)
+        for line in lines[0:2]:
+            print(line, end="")
+        print("")
+        print(lines[2].split(" "))
         gpu = line.split(" ")[0] + " " + line.split(" ")[1].strip(",")
         used = int(line.split(" ")[2])
         free = int(line.split(" ")[4])
-        cap = used + free
-        # if this line does not contain the same values as the previous line
-        if used != mem_used_MiB[-1]:
-            # append variables to respective lists
-            gpus.append(gpu)
-            mem_used_MiB.append(used)
-            mem_free_MiB.append(free)
-            mem_cap_MiB.append(cap)
-            # append unique memory capacity values to its respective list
-            if cap not in mem_cap_unique:
-                mem_cap_unique.append(cap)
+        print(type(gpu), gpu.strip(","))
+        print(type(used), used)
+        print(type(free), free)
+        print("")
+        """
+
+        # put gpu name, used memory, and free memory in a pandas data frame // or in any or list format, doesn't matter
+        columns = ("gpu", "memory_used_MiB", "memory_free_MiB")
+        gpus = []
+        mem_used_MiB = []
+        mem_free_MiB = []
+        mem_cap_MiB = []
+        mem_cap_unique = []
+
+        # first iteration (first line)
+        # extract character values to variables
+        gpu, used, free, cap = process_nvidia_smi_line(lines[1])
+        # append variables to respective lists
+        gpus.append(gpu)
+        mem_used_MiB.append(used)
+        mem_free_MiB.append(free)
+        mem_cap_MiB.append(cap)
+        # append unique memory capacity values to its respective list
+        if cap not in mem_cap_unique:
+            mem_cap_unique.append(cap)
+
+        # print(f"last line:\n {lines[-1]}\n")
+        # 'Tesla V100-SXM2'
+        lines.pop(-1)
+        # print(f"last line:\n {lines[-1]}\n")
+        # 'Tesla V100-SXM2-32GB, 0 MiB, 32500 MiB'
+
+        # all subsequent iterations (lines)
+        for i, line in enumerate(lines[2:]):
+            # extract character values to variables
+            gpu, used, free, cap = process_nvidia_smi_line(line)
+            # if this line does not contain the same values as the previous line
+            if used != mem_used_MiB[-1]:
+                # append variables to respective lists
+                gpus.append(gpu)
+                mem_used_MiB.append(used)
+                mem_free_MiB.append(free)
+                mem_cap_MiB.append(cap)
+                # append unique memory capacity values to its respective list
+                if cap not in mem_cap_unique:
+                    mem_cap_unique.append(cap)
+
+    """ FILE CLOSED """
+
+    """ TESTING
+    for i, cap_unique in enumerate(mem_cap_unique):
+        print(f"gpu: {gpus[0]}\nunique cap {i}: {cap_unique}")
+    print("")
+    """
+
+    # separating the max memory usages
+    VRAM_gpu = []
+    VRAM_usage = []
+    VRAM_free = []
+    VRAM_capacity = []
+    # ... and counting the number of sessions contained in the given nvidia-smi.log file (depending on how 3dunet sessions are run, one log file can contain concatenated nvidia-smi outputs of multiple 3dunet sessions.
+    n_0vram = 0
+    usage_max = -1
+    available_min = -1
+    for i in range(len(gpus)):
+
+        """ TESTING
+        print(f"TESTING\t gpu: {gpus[i]}, mem_used_MiB: {mem_used_MiB[i]}, mem_free_MiB: {mem_free_MiB[i]}, mem_cap_MiB: {mem_cap_MiB[i]}")
+        """
+
+        usage = mem_used_MiB[i]
+        available = mem_free_MiB[i]
+        gpu_i = gpus[i]
+
+        # assumes that during training, the nvidia-smi command was executed before the train3dunet command, therefore writing to the log file before anything was loaded into gpu memory (i.e., memory used is then always 0 in the first few lines of the log file).
+        if usage == 0:
+
+            # handling edge case where multiple lines contain 0 VRAM usage (i.e., the same run needed some time before starting to fill VRAM).
+            if len(VRAM_usage) > 0:
+                if usage == VRAM_usage[-1]:
+                    continue
+
+            # increasing counter of separate runs in this nvidia-smi log
+            n_0vram += 1
+
+            # handling/preparing max and min calculations, etc.
+            usage_max = usage
+            available_min = available
+            VRAM_usage.append(usage_max)
+            VRAM_free.append(available_min)
+
+            VRAM_gpu.append(gpu_i)
+            VRAM_capacity.append(usage_max + available_min)
+
+        else:
+
+            # handling edge case which should not occur if train3dunet & prep. steps are executed correctly.
+            assert len(VRAM_usage) > 0, (f"\n Error: The list 'VRAM_usage' is empty (should not).\n Source problem: The first line in the give nvidia-smi.log file is not '0' for 'mem.used [MiB]'. Please review the file '{arg}' (e.g., just insert a line with 0 for mem.used). This means something went wrong when preparing &/ running train3dunet, most probably a user/developer error.")
+            # this case automatically also handles the case where usage_max and available_min are not yet assigned a log value (i.e., they are -1). This should never happen, anyways, but I might forget something in the future and this point right here is very practical for checking for this mistake (i.e., invalid/absent contents of nvidia-smi.log file).
+
+            # handling max and min calculations, etc.
+            usage_max = max(usage, usage_max)
+            available_min = min(available, available_min)
+            VRAM_usage[-1] = usage_max
+            VRAM_free[-1] = available_min
+
+            VRAM_gpu[-1] = gpu_i
+            VRAM_capacity[-1] = usage_max + available_min
+
+    # handling the edge case, where the nvidia-smi.log file has '0' used VRAM. This can happen when experimenting with train3dunet in interactive slurm sessions.
+    if mem_used_MiB[-1] == 0:
+        n_0vram -= 1
+        VRAM_usage.pop(-1)
+        VRAM_free.pop(-1)
+        VRAM_gpu.pop(-1)
+        VRAM_capacity.pop(-1)
+
+    print("")
+    for i, (gpu_j, vram_used, vram_free, vram_cap) in enumerate(zip(VRAM_gpu, VRAM_usage, VRAM_free, VRAM_capacity)):
+        print(f"session {i}, gpu: {gpu_j}, mem_used_MiB: {vram_used}, mem_free_MiB: {vram_free}, mem_cap_MiB: {vram_cap}.")
 
 
-""" FILE CLOSED """
-
-
-for cap_unique in mem_cap_unique:
-    print(f"gpu: {gpus[0]}\nunique cap: {cap_unique}")
-print("")
-
-n_0vram = 0
-for i in range(len(gpus)):
-    print(f"gpu:\t{gpus[i]},\tmem_used_MiB:\t{mem_used_MiB[i]},\tmem_free_MiB:\t{mem_free_MiB[i]},\tmem_cap_MiB:\t{mem_cap_MiB[i]}")
-    if mem_used_MiB[i] == 0:
-        n_0vram += 1
-n_0vram -= 1  # at the end, the read nvidia-smi log has 0s, so subtracting this last count
-print("")
-print(f"n vram = 0: {n_0vram}")
-    # the n of 0s at the start of an nvidia-smi log entry is 20, the same number of runs on that day (230901, all runs range from -0 to -19), and all these runs' nvidia log files are progressively and consistently smaller. All contain the same last lines.
-print("")
-
-# separating the max memory usages
-max_usage = []
-min_available = []
-VRAM_usage = []
-VRAM_free = []
-for i in range(len(gpus)):
-    usage = mem_used_MiB[i]
-    avail = mem_free_MiB[i]
-
-    if usage == 0:
-        if i == 0:
-            max_usage = [usage]
-            min_available = [avail]
-            continue
-        # calc max and min for this run
-        VRAM_usage.append(max(max_usage))
-        VRAM_free.append(min(min_available))
-        max_usage = [usage]
-        min_available = [avail]
-
+if __name__ == "__main__":
+    # print(sys.argv)
+        # the first element (index 0) in sys.argv is always the absolute file path of the current python script.
+    if len(sys.argv) > 1:
+        for i, arg in enumerate(sys.argv):
+            print(f"\nnvidia-smi-separate_sessions.py: file {i} {arg}")
+            main(arg)
     else:
-        max_usage.append(usage)
-        min_available.append(avail)
-
-for vram_used, vram_free in zip(VRAM_usage, VRAM_free):
-    print(f"used: {vram_used}\nfree: {vram_free}")
+        # for local (Dancer) testing with a default file, write "" as list element.
+        static_arguments = [""]
+        for i, arg in enumerate(static_arguments):
+            if arg == "":
+                print(f"\nnvidia-smi-separate_sessions.py: file {i} '{arg}'")
+                main()
+            else:
+                print(f"\nnvidia-smi-separate_sessions.py: file {i} '{arg}'")
+                main(arg)
