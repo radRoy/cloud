cc6b2ace4bd7d54f8684e24145d482f8851b1360
radRoy <daniel.walther@uzh.ch>
2023-10-08 17:35:38 +0200

    changes test3dunet prep

---
+++ slurmJobs/slurm_job-train3dunet.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --gres=gpu:A100:1
+#SBATCH --time=01:00:00
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=daniel.walther@uzh.ch
+
+# info: do not use `srun` in sbatch scripts, except job step creation is enabled (I think this would be done by setting --ntasks to more than 1)
+
+cd ~/data/cloud  # this line appears to be required for below nvidia-smi logging (writing to file) to work.
+# bash pull-script.sh
+
+# session variables
+session=$1  # "yymmdd-id"
+echo " ${0}: session: ${session}"
+checkdir=$2  # starting with "/home/dwalth/..." without trailing slash
+echo " ${0}: checkdir: ${checkdir}"
+echo " ${0}: slurm output file name: ${3}"
+
+# train3dunet commands
+module load anaconda3
+source activate 3dunet
+
+touch $checkdir/nvidia-smi.log
+nvidia-smi -i $CUDA_VISIBLE_DEVICES -l 2 --query-gpu=gpu_name,memory.used,memory.free --format=csv -f $checkdir/nvidia-smi.log &
+
+touch $checkdir/train3dunet.output
+train3dunet --config ~/data/cloud/pytorch-3dunet/resources/DW-3DUnet_lightsheet_boundary/named_copies/train_config-$session.yml 2>&1 | tee -a $checkdir/train3dunet.output
---
+++ slurmJobs/start_slurm_session-train3dunet copy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# navigating directories and pulling newest committed files
+cd /home/dwalth/data/cloud
+# bash pull-script.sh  # in case that got forgotten
+
+# creating the session string (makes it possible to give a custom session name as input, defaults to current date and a number yymmdd-id)
+# session example: '230930-0'
+if ! [ $# -eq 0 ]; then
+    session=$1
+else
+    source ./getNextSession.sh
+    session=$(nextSession)
+fi
+
+# creating the session's checkpoint directory if necessary
+checkdir="/home/dwalth/data/outputs/chpt-${session}"
+if ! [ -d "$checkdir" ]; then
+    mkdir $checkdir
+fi
+
+# calling the slurm job file (containing the train3dunet command, for example)
+slurmout=$checkdir/slurm-$session.out
+sbatch --output=$slurmout /home/dwalth/data/cloud/slurmJobs/named_copies/slurm_job-$session.sh $session $checkdir $slurmout
+
+# copying outputs (should get executed after the slurm job has finished, for whatever reason)
+### This does not work, currently. Need to find a way to wait for above 'sbatch' process to finish before executing these lines.
+## backup_checkdir="/home/dwalth/data/backup_outputs/chpt-${session}"  # without trailing slash
+## if ! [ -d "$backup_checkdir" ]; then
+##     mkdir $backup_checkdir
+## fi
+## mv $slurmout $checkdir/
+## cp $checkdir/* $backup_checkdir/
