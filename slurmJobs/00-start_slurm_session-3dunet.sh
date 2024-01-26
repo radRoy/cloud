573680f3d20c16b5870f39122a569000bbe9b27e
radRoy <daniel.walther@uzh.ch>
2024-01-26 16:29:41 +0100

    move some slurm scripting files

--- slurmJobs/00-start_slurm_session-3dunet.sh
+++ slurmJobs/00-start_slurm_session-3dunet.sh
@@ -4,24 +4,28 @@
 cd /home/dwalth/data/cloud
 # bash pull-script.sh  # in case that got forgotten
 
-# creating the session string (makes it possible to give a custom session name as input, defaults to current date and a number yymmdd-id)
-# session example: '230930-0'
+# taking the session string input argument, if there is an argument at all. e.g.: '240124-0'
 if ! [ $# -eq 0 ]; then
-    session=$1
+    session_argument=$1
 else
-    source ./getNextSession.sh
-    session=$(nextSession)
+    session_argument=""
 fi
 
-# creating the session's checkpoint directory if necessary
-checkdir="/home/dwalth/data/outputs/chpt-${session}"
-if ! [ -d "$checkdir" ]; then
-    mkdir $checkdir
-fi
+# creating the (current) session string (makes it possible to give a custom session name as input, defaults to current date and a number yymmdd-id)
+# session example: '230930-0'
+source ./getNextSession.sh
+session=$(nextSession)
 
-# calling the slurm job file (containing the train3dunet command, for example)
-slurmout=$checkdir/slurm-$session.out
-sbatch --output=$slurmout /home/dwalth/data/cloud/slurmJobs/named_copies/slurm_job-$session.sh $session $checkdir $slurmout
+if [session_argument -eq "" -o]
+    # creating the session's checkpoint directory if necessary
+    checkdir="/home/dwalth/data/outputs/chpt-${session}"
+    if ! [ -d "$checkdir" ]; then
+        mkdir $checkdir
+    fi
+
+    # calling the slurm job file (containing the train3dunet command, for example)
+    slurmout=$checkdir/slurm-$session.out
+    sbatch --output=$slurmout /home/dwalth/data/cloud/slurmJobs/named_copies/slurm_job-$session.sh $session $checkdir $slurmout
 
 # copying outputs (should get executed after the slurm job has finished, for whatever reason)
 ### This does not work, currently. Need to find a way to wait for above 'sbatch' process to finish before executing these lines.
