1cd4091985ec0027068d393b7aff8a02f8987b54
radRoy <daniel.walther@uzh.ch>
2024-01-31 20:19:25 +0100

    prepare predict3dunet 1.8.2 chpt-240131-0 to -7

---
+++ slurmJobs/named_copies_arrays/start_slurm_sessions-240131-predict.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# navigating directories
+cd /home/dwalth/data/cloud
+
+# running all the slurm starter scripts
+echo "command: bash ~/data/cloud/bash_scripts/slurm/00-start_slurm_session-3dunet.sh 240124-0"
+bash ~/data/cloud/bash_scripts/slurm/00-start_slurm_session-3dunet.sh 240124-0
+
+echo "command: bash ~/data/cloud/bash_scripts/slurm/00-start_slurm_session-3dunet.sh 240124-0"
+bash ~/data/cloud/bash_scripts/slurm/00-start_slurm_session-3dunet.sh 240124-0
+
+echo "command: bash ~/data/cloud/bash_scripts/slurm/00-start_slurm_session-3dunet.sh 240125-0"
+bash ~/data/cloud/bash_scripts/slurm/00-start_slurm_session-3dunet.sh 240125-0
+
+echo "command: bash ~/data/cloud/bash_scripts/slurm/00-start_slurm_session-3dunet.sh 240125-0"
+bash ~/data/cloud/bash_scripts/slurm/00-start_slurm_session-3dunet.sh 240125-0
+
+echo "command: bash ~/data/cloud/bash_scripts/slurm/00-start_slurm_session-3dunet.sh 240125-5"
+bash ~/data/cloud/bash_scripts/slurm/00-start_slurm_session-3dunet.sh 240125-5
+
+echo "command: bash ~/data/cloud/bash_scripts/slurm/00-start_slurm_session-3dunet.sh 240125-5"
+bash ~/data/cloud/bash_scripts/slurm/00-start_slurm_session-3dunet.sh 240125-5
+
+echo "command: bash ~/data/cloud/bash_scripts/slurm/00-start_slurm_session-3dunet.sh 240125-7"
+bash ~/data/cloud/bash_scripts/slurm/00-start_slurm_session-3dunet.sh 240125-7
+
+echo "command: bash ~/data/cloud/bash_scripts/slurm/00-start_slurm_session-3dunet.sh 240125-7"
+bash ~/data/cloud/bash_scripts/slurm/00-start_slurm_session-3dunet.sh 240125-7
