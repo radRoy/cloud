c5f2ad1512107f19cbfe90577946bd47085d223b
radRoy <daniel.walther@uzh.ch>
2024-02-02 22:01:19 +0100

    generic commit message, Fri 22:01:19 02.02.2024.

--- slurmJobs/named_copies/sbatch-240202-2.sh
+++ slurmJobs/named_copies/sbatch-240202-2.sh
@@ -3,7 +3,7 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32G
 #SBATCH --gres=gpu:A100:1
-#SBATCH --time=00:30:00
+#SBATCH --time=6:00:00
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=daniel.walther@uzh.ch
 
@@ -12,38 +12,39 @@
 cd ~/data/cloud  # this line appears to be required for below nvidia-smi logging (writing to file) to work.
 # bash pull-script.sh
 
-if [ $# -lt 4 ]; then
-    echo "Error. Input session expected. Input session (folder containing the pytorch checkpoint) is required with my starter script."
+if [ $# -gt 3 ]; then
+    echo Error. Input no input session expected. Input session with my train3dunet starter script is not supported.
     exit
 fi
 
 # session variables
-input_session=$1
-echo " ${0}: input_session: ${input_session}"
-session=$2  # "yymmdd-id"
+session=$1  # "yymmdd-id"
 echo " ${0}: session: ${session}"
-checkdir=$3  # starting with "/home/dwalth/..." without trailing slash
-echo " ${0}: checkdir: ${checkdir}"
-output_dir=$checkdir/$session
-mkdir $output_dir  # throws an error if it already exists
-echo " ${0}: output_dir: ${output_dir}"
-echo " ${0}: slurm output file name: ${4}"
+checkdir=$2  # starting with "/home/dwalth/..." without trailing slash
+output_dir=$checkdir
+echo " ${0}: checkdir (= output_dir): ${checkdir}"
+echo " ${0}: slurm output file name: ${3}"
 
 # 3dunet commands
 #module load mamba  # using mamba not conda, 3dunet1.8.2 was built using mamba, maybe it makes a difference, now.
 module load mamba/23.3.1-1  # above line causes an error `Lmod has detected the following error: The following module(s) are unknown: "mamba"`, this results in the end in a gcc related error.
 source activate 3dunet1.8.2
 
+echo "command: nvidia_log=$output_dir/nvidia-smi-$session.log"
 nvidia_log=$output_dir/nvidia-smi-$session.log
 touch $nvidia_log
+
+echo "command: nvidia-smi -i \$CUDA_VISIBLE_DEVICES -l 2 --query-gpu=gpu_name,memory.used,memory.free --format=csv -f \$nvidia_log &"
 nvidia-smi -i $CUDA_VISIBLE_DEVICES -l 2 --query-gpu=gpu_name,memory.used,memory.free --format=csv -f $nvidia_log &
 
-# predict3dunet preparation
-predict3dunet_output=$output_dir/predict3dunet-$session.output
-touch $predict3dunet_output
-config=/home/dwalth/data/cloud/configs/named_copies/test_config-$session.yml
-cp $config $output_dir/
-cp ~/$(dirname $0)/$0 $output_dir/  # works
+# train3dunet preparation
+echo: "command: train3dunet_output=$output_dir/train3dunet-$session.output"
+train3dunet_output=$output_dir/train3dunet-$session.output
+touch $train3dunet_output
 
-# predict3dunet command
-predict3dunet --config $config 2>&1 | tee -a $predict3dunet_output
+config=/home/dwalth/data/cloud/configs/named_copies/train_config-$session.yml
+cp $config $output_dir/
+cp $(dirname $0)/$0 $output_dir/  # works
+
+# train3dunet command
+train3dunet --config $config 2>&1 | tee -a $train3dunet_output
